778ebd947640fa50375f964d88f3fe5c9d9b50e1
sameersul <sameerjuin@gmail.com>
2024-09-04 11:15:20 -0400

    made website more nicer

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -45,14 +45,6 @@
                 <p>{tab.name}</p>
             </a>
         {/each}
-        <button
-            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950 cursor-hover"
-        >
-            <div
-                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-            />
-            <h4 class="relative z-9">Contact Me</h4>
-        </button>
     </div>
 </header>
 
